400b513eed643b749fe97558dfeedaf6e83905c1
Aaron <ztoplin@yahoo.com>
2022-12-16 14:01:09 -0500

    commit tree 5e8517ae04b046ddf6bde0bbcd94fa36c4fb333a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait? 
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
 end
